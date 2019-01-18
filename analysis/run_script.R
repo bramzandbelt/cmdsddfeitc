ba0bc584a3b75ef2a41340138d14bd04029dbdb6
Bram Zandbelt <bramzandbelt@gmail.com>
2019-01-18 11:07:43 +0100

    Make notebook rendering comp_job-dependent, so that parameters are loaded from file when comp_job equals "vis"

--- analysis/run_script.R
+++ analysis/run_script.R
@@ -60,43 +60,43 @@ if (notebook_ix == 1) {
   } else if (comp_job == "vis") {
     do_optimize = FALSE
     do_visualize = TRUE
-
-    # # Load best-fitting parameters from file
-    #
-    # data_dir <- NA
-    #
-    #
-    # par_vals_file <-
-    #   file.path(data_output_dir,
-    #             sprintf("best_fitting_params_task-.*_pid-%.3d_model-%s_pmz-%s_bounds-%s_BIC-%.0f.csv",
-    #                     ,
-    #                     pid, # pid
-    #                     model_name, # mode
-    #                     parameterization, # pmz
-    #                     bound_setting, # bounds
-    #                     optim_stats$BIC # BIC
-    #             )
-    #   )
-
   } else {
     do_optimize = TRUE
     do_visualize = TRUE
   }
 
-  cmdsddfeitc::render_notebook(notebook_file = "03_computational_modeling_analysis.Rmd",
-                               notebook_dir = "analysis",
-                               reports_dir = "reports/03_computational_modeling_analysis",
-                               params_tibble = tidyr::crossing(participant_id = pid,
-                                                               model_name = model_name,
-                                                               parameterization = parameterization,
-                                                               bound_setting = "wide",
-                                                               algorithm = algorithm,
-                                                               max_iter = max_iter,
-                                                               rel_tol = 0.000001,
-                                                               n_pop_per_free_param = 10,
-                                                               optimize = do_optimize,
-                                                               visualize = do_visualize),
-                               force = TRUE)
+  if (comp_job == "vis") {
+    cmdsddfeitc::render_notebook(notebook_file = "03_computational_modeling_analysis.Rmd",
+                                 notebook_dir = "analysis",
+                                 reports_dir = "reports/03_computational_modeling_analysis",
+                                 params_tibble = tidyr::crossing(participant_id = pid,
+                                                                 model_name = model_name,
+                                                                 parameterization = parameterization,
+                                                                 bound_setting = "wide",
+                                                                 algorithm = algorithm,
+                                                                 max_iter = max_iter,
+                                                                 rel_tol = 0.000001,
+                                                                 n_pop_per_free_param = 10,
+                                                                 optimize = do_optimize,
+                                                                 visualize = do_visualize,
+                                                                 pars_from_file = TRUE),
+                                 force = TRUE)
+    } else {
+      cmdsddfeitc::render_notebook(notebook_file = "03_computational_modeling_analysis.Rmd",
+                                   notebook_dir = "analysis",
+                                   reports_dir = "reports/03_computational_modeling_analysis",
+                                   params_tibble = tidyr::crossing(participant_id = pid,
+                                                                   model_name = model_name,
+                                                                   parameterization = parameterization,
+                                                                   bound_setting = "wide",
+                                                                   algorithm = algorithm,
+                                                                   max_iter = max_iter,
+                                                                   rel_tol = 0.000001,
+                                                                   n_pop_per_free_param = 10,
+                                                                   optimize = do_optimize,
+                                                                   visualize = do_visualize),
+                                   force = TRUE)
+      }
 }
 
 
