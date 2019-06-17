83247dff024fc02f92267d887be7262a9c01a921
Bram Zandbelt <bramzandbelt@gmail.com>
2019-06-17 07:03:51 +0200

    Remove obsolete or renamed files

--- analysis/01_preprocessing.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash
-#
-# 01_preprocessing.sh
-
-nb_ix=1;
-pid=$1;
-
-Rscript $PWD/analysis/run_script.R $nb_ix $pid
--- analysis/02_exploratory_data_analysis.sh
+++
@@ -1,8 +0,0 @@
-# !/bin/bash
-#
-# 02_exploratory_data_analysis.sh
-
-nb_ix=2;
-pid=$1;
-
-Rscript $PWD/analysis/run_script.R $nb_ix $pid
