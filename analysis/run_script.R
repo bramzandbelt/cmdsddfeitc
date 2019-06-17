ca586a1ffca04c37769b417aa925f6e6706e43e3
Bram Zandbelt <bramzandbelt@gmail.com>
2019-06-17 07:09:39 +0200

    Add new notebooks for processing to run_script and restructure code

--- analysis/run_script.R
+++ analysis/run_script.R
@@ -1,38 +1,44 @@
 require(tidyverse)
+require(DT)
 library(cmdsddfeitc)
 
+# Provides access to a copy of the command line arguments, which specifies what to do
 args <- commandArgs(TRUE)
 
+# Get index of notebook to render to identify notebook filename
 notebook_ix <- as.integer(args[1])
 
-notebook_file <- switch(notebook_ix,
-                        "1" = "01_preprocessing.Rmd",
-                        "2" = "02_exploratory_data_analysis.Rmd",
-                        "3" = "03_computational_modeling_analysis.Rmd")
+notebook_file <-
+  switch(notebook_ix,
+         "1" = "01_preprocessing_idv.Rmd",
+         "2" = "02_eda_idv.Rmd",
+         "3" = "03_computational_modeling_analysis_idv.Rmd",
+         "4" = "04_sanity_check_control_trial_performance_grp.Rmd",
+         "5" = "05_eda_grp.Rmd",
+         "6" = "06_model_comparison_grp.Rmd",
+         "7" = "07_observed_vs_predicted_performance_grp.Rmd",
+         "8" = "08_analysis_of_model_parameters_grp.Rmd",
+         "9" = "09_sanity_check_effect_framing_on_model_predicted_auc_grp.Rmd"
+         )
 
+# Define input dir (notebook_templates_dir) and outpur dir (reports_dir)
+notebook_templates_dir <- "analysis/notebook_templates"
+reports_dir <- file.path("reports", stringr::str_replace(notebook_file,
+                                                         pattern = ".Rmd",
+                                                         replacement = ""))
+
+# Specify notebook parameters based on notebook index --------------------------
 
 if (notebook_ix == 1) {
-  # Participant ID
-  pid <- as.integer(args[2])
 
-  cmdsddfeitc::render_notebook(notebook_file = "01_preprocessing.Rmd",
-                               notebook_dir = "analysis",
-                               reports_dir = "reports/01_preprocessing",
-                               params_tibble = tidyr::crossing(participant_id = pid,
-                                                               visualize = TRUE),
-                               force = TRUE)
+  params_tibble <-
+    tidyr::crossing(participant_id = as.integer(args[2]),
+                    visualize = TRUE)
 
 } else if (notebook_ix == 2) {
-  # Participant ID
-  pid <- as.integer(args[2])
-
-  cmdsddfeitc::render_notebook(notebook_file = "02_exploratory_data_analysis.Rmd",
-                               notebook_dir = "analysis",
-                               reports_dir = "reports/02_exploratory_data_analysis",
-                               params_tibble = tidyr::crossing(participant_id = pid,
-                                                               visualize = TRUE),
-                               force = TRUE)
-
+  params_tibble <-
+    tidyr::crossing(participant_id = as.integer(args[2]),
+                    visualize = TRUE)
 } else if (notebook_ix == 3) {
 
   pid <- as.integer(args[2])
@@ -66,37 +72,71 @@ if (notebook_ix == 1) {
   }
 
   if (comp_job == "vis") {
-    cmdsddfeitc::render_notebook(notebook_file = "03_computational_modeling_analysis.Rmd",
-                                 notebook_dir = "analysis",
-                                 reports_dir = "reports/03_computational_modeling_analysis",
-                                 params_tibble = tidyr::crossing(participant_id = pid,
-                                                                 model_name = model_name,
-                                                                 parameterization = parameterization,
-                                                                 bound_setting = "wide",
-                                                                 algorithm = algorithm,
-                                                                 max_iter = max_iter,
-                                                                 rel_tol = 0.000001,
-                                                                 n_pop_per_free_param = 10,
-                                                                 optimize = do_optimize,
-                                                                 visualize = do_visualize,
-                                                                 pars_from_file = TRUE),
-                                 force = TRUE)
-    } else {
-      cmdsddfeitc::render_notebook(notebook_file = "03_computational_modeling_analysis.Rmd",
-                                   notebook_dir = "analysis",
-                                   reports_dir = "reports/03_computational_modeling_analysis",
-                                   params_tibble = tidyr::crossing(participant_id = pid,
-                                                                   model_name = model_name,
-                                                                   parameterization = parameterization,
-                                                                   bound_setting = "wide",
-                                                                   algorithm = algorithm,
-                                                                   max_iter = max_iter,
-                                                                   rel_tol = 0.000001,
-                                                                   n_pop_per_free_param = 10,
-                                                                   optimize = do_optimize,
-                                                                   visualize = do_visualize),
-                                   force = TRUE)
-      }
+    params_tibble <-
+      tidyr::crossing(participant_id = pid,
+                      model_name = model_name,
+                      parameterization = parameterization,
+                      bound_setting = "wide",
+                      algorithm = algorithm,
+                      max_iter = max_iter,
+                      rel_tol = 0.000001,
+                      n_pop_per_free_param = 20,
+                      optimize = do_optimize,
+                      visualize = do_visualize,
+                      pars_from_file = TRUE)
+  } else {
+    params_tibble <-
+      tidyr::crossing(participant_id = pid,
+                      model_name = model_name,
+                      parameterization = parameterization,
+                      bound_setting = "wide",
+                      algorithm = algorithm,
+                      max_iter = max_iter,
+                      rel_tol = 0.000001,
+                      n_pop_per_free_param = 20,
+                      optimize = do_optimize,
+                      visualize = do_visualize)
+  }
+} else if (notebook_ix == 4) {
+
+  params_tibble <-
+    tidyr::crossing(task = args[2])
+
+} else if (notebook_ix == 5) {
+
+  params_tibble <-
+    tidyr::crossing(task = args[2])
+
+} else if (notebook_ix == 6) {
+
+  params_tibble <-
+    tidyr::crossing(task = args[2],
+                    algorithm = args[3])
+
+} else if (notebook_ix == 7) {
+
+  params_tibble <-
+    tidyr::crossing(task = args[2],
+                    algorithm = args[3])
+
+} else if (notebook_ix == 8) {
+
+  params_tibble <-
+    tidyr::crossing(task = args[2],
+                    algorithm = args[3])
+
+} else if (notebook_ix == 9) {
+
+  params_tibble <-
+    tidyr::crossing(task = args[2],
+                    algorithm = args[3])
+
 }
 
+# Render notebook into static HTML file ----------------------------------------
+cmdsddfeitc::render_notebook(notebook_file = notebook_file,
+                             notebook_dir = notebook_templates_dir,
+                             reports_dir = reports_dir,
+                             params_tibble = params_tibble,
+                             force = TRUE)
 
